41f11625952613a6f4d4d432d41c544c2738118c
masaki <robomasaki2@gmail.com>
2020-10-14 16:40:06 +0900

    added simple notification

--- app/src/main/java/com/example/notifycation/FirstFragment.kt
+++ app/src/main/java/com/example/notifycation/FirstFragment.kt
@@ -1,27 +1,76 @@
 package com.example.notifycation
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.core.app.NotificationCompat
+import androidx.fragment.app.Fragment
+import com.example.notifycation.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    private lateinit var binding: FragmentFirstBinding
+    private val CHANNEL_ID = "simple-notification-channel"
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        binding.simpleNotification.setOnClickListener {
+            val notification = createSimpleNotification()
+            showNotification(notification)
+        }
+    }
+
+    private fun createSimpleNotification(): Notification {
+        val builder = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications_24px)
+            .setContentTitle("Notification Title")
+            .setContentText("Notification Content Text.\nHello from Fragment")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        return builder.build()
+    }
+
+    // NotificationManagerに対して,通知を渡すことで表示できる
+    private fun showNotification(notification: Notification) {
+        createNotificationChannel()
+        val notificationManager =
+            requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(R.string.app_name, notification)
+    }
+
+    // Android 8.0以上で通知を配信するには、
+    // NotificationChannel のインスタンスを createNotificationChannel() に渡すことにより、
+    // アプリの通知チャネルをシステムに登録しておく必要があります。
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+                .apply { description = descriptionText }
+
+            // 通知チャンネルをシステムに登録
+            val notificationManager =
+                requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/notifycation/MainActivity.kt
+++ app/src/main/java/com/example/notifycation/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.notifycation
 
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
