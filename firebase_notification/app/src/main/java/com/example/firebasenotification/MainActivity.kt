8a2df4173ac4c7c8e414ff51e493e93574776b8c
masaki <robomasaki2@gmail.com>
2020-10-20 20:58:41 +0900

    added cloud messaging test function

--- firebase_notification/app/src/main/java/com/example/firebasenotification/MainActivity.kt
+++ firebase_notification/app/src/main/java/com/example/firebasenotification/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.firebasenotification
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasenotification.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.accessRegisteredToken.setOnClickListener { getToken() }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private fun getToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
 }
