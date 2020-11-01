ab7d06f4275c1deb053bd5f73a74eea989784ac7
masaki <robomasaki2@gmail.com>
2020-11-01 20:14:36 +0900

    Added adaptive banner

--- admob/app/src/main/java/com/example/admob/BannerActivity.kt
+++ admob/app/src/main/java/com/example/admob/BannerActivity.kt
@@ -2,10 +2,11 @@ package com.example.admob
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import com.example.admob.databinding.ActivityBannerBinding
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
 
 class BannerActivity : Activity() {
     private lateinit var binding: ActivityBannerBinding
@@ -23,11 +24,43 @@ class BannerActivity : Activity() {
             loadAd(fullBanner)
             loadAd(leaderBoard)
             loadAd(smartBanner)
+            setupAdaptiveBanner()
         }
     }
 
     private fun loadAd(adView: AdView) {
         val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = binding.adViewContainer.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
+
+    private fun setupAdaptiveBanner() {
+        // FrameLayoutにAdViewをセット
+        val adView = AdView(this)
+        binding.adViewContainer.addView(adView)
+        // AdViewの設定
+        adView.adUnitId = "ca-app-pub-3940256099942544/6300978111"
+        adView.adSize = adSize
+
+        val adRequest = AdRequest
+            .Builder()
+            .build()
         adView.loadAd(adRequest)
     }
 }
