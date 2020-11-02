7fd548f54b471e8ae769d697487000f069724bbb
masaki <robomasaki2@gmail.com>
2020-11-02 13:34:33 +0900

    added worker requests

--- work_manager/app/src/main/java/com/example/workmanager/MainActivity.kt
+++ work_manager/app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.workmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.WorkManager
+import com.example.workmanager.workrequest.DataWorkRequest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // WorkとWorkRequestを定義し、
+        // 処理をキューに登録する
+        WorkManager
+            .getInstance(applicationContext)
+            .enqueue(DataWorkRequest.createWorkRequest())
+
     }
 }
