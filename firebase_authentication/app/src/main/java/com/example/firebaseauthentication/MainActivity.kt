4ffd6ddf68edfdafe72f71e3232e8c37f81d71b9
masaki <robomasaki2@gmail.com>
2020-11-03 14:35:42 +0900

    Implemented google sign in

--- firebase_authentication/app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
+++ firebase_authentication/app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
@@ -1,11 +1,122 @@
 package com.example.firebaseauthentication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.firebaseauthentication.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            signInButton.setOnClickListener { signIn() }
+            signOutButton.setOnClickListener { signOut() }
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) updateUI(currentUser)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            RC_SIGN_IN -> {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                    signInGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    Log.w(TAG, "Google sign in failed", e)
+                }
+            }
+        }
+    }
+
+    /**
+     * Googleサインイン
+     */
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        googleSignInClient
+            .signOut()
+            .addOnCompleteListener { clearUI() }
+    }
+
+    private fun signInGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) onSignInCompleted()
+                else onSignInFailed()
+            }
+    }
+
+    /**
+     * サインインの状態を通知
+     */
+    private fun onSignInCompleted() {
+        Toast.makeText(this, "Sign In Success", Toast.LENGTH_LONG).show()
+        val user = auth.currentUser
+        if (user != null) updateUI(user)
+    }
+
+    private fun onSignInFailed() {
+        Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+        clearUI()
+    }
+
+    /**
+     * UI関連のコード
+     */
+    private fun updateUI(user: FirebaseUser) {
+        binding.userName.text = user.displayName
+        Glide.with(this)
+            .load(user.photoUrl)
+            .override(500)
+            .circleCrop()
+            .into(binding.userImage)
+    }
+    private fun clearUI(){
+        binding.userImage.setImageResource(0)
+        binding.userName.text = ""
     }
 }
