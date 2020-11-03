a95a07351bd1fdfd6fa4970238d9fcc678aeb698
masaki <robomasaki2@gmail.com>
2020-11-03 16:37:50 +0900

    added layout for landscape tablet

--- adaptive_uI/app/src/main/java/com/example/adaptiveui/FirstFragment.kt
+++ adaptive_uI/app/src/main/java/com/example/adaptiveui/FirstFragment.kt
@@ -7,24 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.adaptiveui.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        
+        binding.buttonFirst?.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
