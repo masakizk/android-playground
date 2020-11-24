8eeb3ec10ff2093af06e4978f6310aa1ade87a24
masaki <robomasaki2@gmail.com>
2020-11-24 15:22:37 +0900

    feat(ui): added preference listeners

--- preferences/app/src/main/java/com/example/preferences/MainActivity.kt
+++ preferences/app/src/main/java/com/example/preferences/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.preferences
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.example.preferences.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,41 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(binding.fragmentContainer.id, SettingsFragment())
             .commit()
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        loadPreferences()
+    }
+
+    /**
+     * ライフサイクルを適切に管理するために、リスナーの登録と解除を
+     * onResume(), onPause()コールバックで行う
+     */
+    override fun onResume() {
+        super.onResume()
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    private fun loadPreferences() {
+        val key = getString(R.string.saved_boolean_value)
+        val savedValue = sharedPreferences.getBoolean(key, true)
+
+        Toast.makeText(this, "key: $key, value:$savedValue", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        val savedKey = getString(R.string.saved_boolean_value)
+        val savedValue = sharedPreferences.getBoolean(savedKey, true)
+        Toast.makeText(
+            this,
+            "[onSharedPreferenceChanged]\n key: saved_boolean_value \nvalue:${savedValue}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
--- preferences/app/src/main/java/com/example/preferences/SettingsFragment.kt
+++ preferences/app/src/main/java/com/example/preferences/SettingsFragment.kt
@@ -1,11 +1,21 @@
 package com.example.preferences
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.CheckBoxPreference
 import androidx.preference.Preference
 
-class SettingsFragment : BasePreferenceFragment() {
+class SettingsFragment : BasePreferenceFragment(), Preference.OnPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val key = getString(R.string.saved_boolean_value)
+        val checkbox = findPreference<CheckBoxPreference>(key)
+        checkbox?.onPreferenceChangeListener = this
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
@@ -16,6 +26,15 @@ class SettingsFragment : BasePreferenceFragment() {
             else -> return super.onPreferenceTreeClick(preference)
         }
 
+        return true
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        Toast.makeText(
+            requireContext(),
+            "[onPreferenceChange] \nkey:${preference.key} \nvalue:$newValue",
+            Toast.LENGTH_LONG
+        ).show()
         return true
     }
 }
