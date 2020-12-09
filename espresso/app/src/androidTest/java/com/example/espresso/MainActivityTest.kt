32c039519ae2fd6128293c02d80c32f92a48ba27
masaki <robomasaki2@gmail.com>
2020-12-09 15:47:26 +0900

    test(mainactivity): simple ui test

---
+++ espresso/app/src/androidTest/java/com/example/espresso/MainActivityTest.kt
@@ -0,0 +1,34 @@
+package com.example.espresso
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun helloWorldText() {
+        onView(withId(R.id.text_hello_world))
+            .check(matches(withText("Hello World!")))
+    }
+
+    @Test
+    fun changeText(){
+        // Click button
+        onView(withId(R.id.button_change_message)).perform(click())
+        // Text will change
+        onView(withId(R.id.text_hello_world)).check(matches(withText("Espresso")))
+    }
+}
--- espresso/app/src/main/java/com/example/espresso/MainActivity.kt
+++ espresso/app/src/main/java/com/example/espresso/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.espresso
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_change_message)
+            .setOnClickListener {
+                // ボタンを押されたら、テキストを更新
+                findViewById<TextView>(R.id.text_hello_world).text = "Espresso"
+            }
     }
 }
