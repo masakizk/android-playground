356dd27bbfdbee8330df076cc2366705b1694fb1
masaki <robomasaki2@gmail.com>
2020-12-09 16:15:09 +0900

    test(mainactivity): edit text test

--- espresso/app/src/androidTest/java/com/example/espresso/MainActivityTest.kt
+++ espresso/app/src/androidTest/java/com/example/espresso/MainActivityTest.kt
@@ -1,7 +1,7 @@
 package com.example.espresso
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -30,5 +30,14 @@ class MainActivityTest {
         onView(withId(R.id.button_change_message)).perform(click())
         // Text will change
         onView(withId(R.id.text_hello_world)).check(matches(withText("Espresso")))
+    }
+
+    @Test
+    fun setMessage() {
+        // set message and close keyboard
+        onView(withId(R.id.et_message)).perform(typeText("Android"), closeSoftKeyboard())
+        // change text
+        onView(withId(R.id.button_change_message)).perform(click())
+        onView(withId(R.id.text_hello_world)).check(matches(withText("Android")))
     }
 }
