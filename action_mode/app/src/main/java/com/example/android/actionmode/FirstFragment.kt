86104ac2d9ce89786290fb432a0eb3ac8ab5758b
ishizakamasaki <robomasaki2@gmail.com>
2021-02-08 09:49:35 +0900

    feat: action mode
    Primary Action ModeとFloating Action Modeの２つのサンプルを実装した。

--- action_mode/app/src/main/java/com/example/android/actionmode/FirstFragment.kt
+++ action_mode/app/src/main/java/com/example/android/actionmode/FirstFragment.kt
@@ -1,31 +1,74 @@
 package com.example.android.actionmode
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import com.example.android.actionmode.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private lateinit var binding: FragmentFirstBinding
+
+    private val actionModeCallback = object : ActionMode.Callback {
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            mode?.menuInflater?.inflate(R.menu.menu_action_mode, menu)
+            return true
+        }
+
+        // 更新時の処理
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            return false
+        }
+
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            return when (item?.itemId) {
+                R.id.done -> {
+                    mode?.finish()
+                    true
+                }
+                R.id.close -> {
+                    mode?.finish()
+                    true
+                }
+                else -> true
+            }
+        }
+
+        // 破棄後の処理
+        override fun onDestroyActionMode(mode: ActionMode?) {
+
+        }
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+    ): View {
+        binding = FragmentFirstBinding.inflate(inflater, container, false).apply {
+            activityPrimaryActionMode.setOnClickListener { showActivityPrimaryActionMode() }
+            activityFloatingActionMode.setOnClickListener { showActivityFloatingActionMode() }
+            viewPrimaryActionMode.setOnClickListener { showViewPrimaryActionMode(viewPrimaryActionMode) }
+            viewFloatingActionMode.setOnClickListener { showViewFloatingActionMode(viewFloatingActionMode) }
+        }
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun showActivityPrimaryActionMode() {
+        requireActivity().startActionMode(actionModeCallback, ActionMode.TYPE_PRIMARY)
+    }
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    private fun showActivityFloatingActionMode() {
+        requireActivity().startActionMode(actionModeCallback, ActionMode.TYPE_FLOATING)
+    }
+
+    private fun showViewPrimaryActionMode(view: View) {
+        view.startActionMode(actionModeCallback, ActionMode.TYPE_PRIMARY)
+    }
+
+    private fun showViewFloatingActionMode(view: View) {
+        view.startActionMode(actionModeCallback, ActionMode.TYPE_FLOATING)
     }
 }
